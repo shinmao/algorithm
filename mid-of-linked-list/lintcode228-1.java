780d9e7a9a77e27a87502f707a6cda0811de534e
shinmao <shinmaox2016@gmail.com>
2020-03-19 01:37:08 -0400

    add mid of linked list

---
+++ mid-of-linked-list/lintcode228-1.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: the head of linked list.
+     * @return: a middle node of the linked list
+     */
+    public ListNode middleNode(ListNode head) {
+        // write your code here
+        if(head == null || head.next == null || head.next.next == null){
+            return head;
+        }
+        
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
